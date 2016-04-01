17c9ada7ef3e2e7a8929256af3869b8bea04e7f9
Eric Kelly <heroiceric@gmail.com>
2016-04-01 16:12:28 -0700

    Prevent missing entity name error on install

--- blueprints/ember-markdown-it/index.js
+++ blueprints/ember-markdown-it/index.js
@@ -1,5 +1,9 @@
 /*jshint node:true*/
 module.exports = {
+  normalizeEntityName: function() {
+    // no-op since we're just adding dependencies
+  },
+
   afterInstall: function() {
     return this.addBowerPackageToProject('markdown-it');
   }
